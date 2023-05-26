666e36c8b45406cf130854863c5a77893c8aa5fd
donfortune <donfortunet.df@gmail.com>
2023-05-26 09:58:36 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from rembg import remove
+from PIL import Image
+
+input_path = 'rita.jpg'
+output_path = 'rita_res.png'
+
+# Open the image
+image = Image.open(input_path)
+
+# Remove the image background using rembg library
+processed_image = remove(image)
+
+# Save the processed image as PNG
+processed_image.save(output_path, 'PNG')
