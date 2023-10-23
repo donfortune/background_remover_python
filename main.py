5dc5d91daa91c6ef3b0d786e6ffceeec6aab6e27
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-10-23 21:37:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from PIL import Image
 input_path = 'rita.jpg'
 output_path = 'rita_newres.png'
 
-# Open the image
+
 image = Image.open(input_path)
 
-# Remove the image background using rembg library
+
 processed_image = remove(image)
 
-# Save the processed image as PNG
+
 processed_image.save(output_path, 'PNG')
